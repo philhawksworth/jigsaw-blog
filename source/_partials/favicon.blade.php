ddd3e4ad4524c4b17c3de6cb498b03faa9996f8a
Ranie Santos <ranietots32@gmail.com>
2018-04-07 01:37:39 +0800

    MInor changes, added comments in favicon partial

--- source/_partials/favicon.blade.php
+++ source/_partials/favicon.blade.php
@@ -1,3 +1,4 @@
+<!-- favicon -->
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -5,3 +6,4 @@
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f4645f">
 <meta name="msapplication-TileColor" content="#f1362f">
 <meta name="theme-color" content="#ffffff">
+<!-- end favicon -->
