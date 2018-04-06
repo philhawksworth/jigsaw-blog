63aa6f4b9a397c4722c46ceae6f56aab5e61f201
Ranie Santos <ranietots32@gmail.com>
2018-04-07 01:49:26 +0800

    Added site details to config

--- config.php
+++ config.php
@@ -1,7 +1,17 @@
 <?php
 
 return [
-    'baseUrl' => '',
+    'site' => [
+        'title' => 'Ranie Santos',
+        'description' => 'THE SITE DESCRIPTION', // change this
+        'image' => 'https://some-external-cdn.com/share.png', // change this
+        'owner' => [
+            'name' => 'Ranie Santos',
+            'twitter' => 'https://twitter.com/raniesantos32',
+            'github' => 'https://github.com/raniesantos',
+        ],
+    ],
+    'baseUrl' => 'https://raniesantos.github.io',
     'production' => false,
     'collections' => [],
 ];
