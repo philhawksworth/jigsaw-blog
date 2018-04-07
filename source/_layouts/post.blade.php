702b7cb40891186b6fb35f8426a3e31f7cc5b11d
Ranie Santos <ranietots32@gmail.com>
2018-04-07 17:05:00 +0800

    Added hero image to post template

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -3,9 +3,25 @@
 @section('title', $page->title)
 
 @section('content')
-    <section class="section content" v-pre>
-        <h1>{{ $page->title }}</h1>
-        <strong>{{ date('F j, Y', $page->date) }}</strong>
-        @yield('postContent')
+    @if ($page->image)
+        <section class="hero hero-image overlay-dark is-medium"
+            style="background-image: url(https://raniesantos.github.io/assets/img/post-img/{{ $page->image }});"
+        >
+            <div class="hero-body has-text-centered">
+                <div class="container">
+                    <h1 class="title">{{ $page->title }}</h1>
+                </div>
+            </div>
+        </section>
+    @endif
+
+    <section class="section">
+        <div class="container content" v-pre>
+            @if (!$page->image)
+                <h1>{{ $page->title }}</h1>
+            @endif
+            <strong>{{ date('F j, Y', $page->date) }}</strong>
+            @yield('postContent')
+        </div>
     </section>
 @endsection
