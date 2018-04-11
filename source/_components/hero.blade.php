c5b101f1e9c3768561b8d58c1e2a890706d83329
Ranie Santos <ranietots32@gmail.com>
2018-04-11 15:05:12 +0800

    Refactored scss variable structure

--- source/_components/hero.blade.php
+++ source/_components/hero.blade.php
@@ -10,6 +10,9 @@
     <div class="hero-body{{ $centered ? ' has-text-centered' : '' }}">
         <div class="container">
             <h1 class="title">{{ $title }}</h1>
+            @if ($subtitle)
+                <h2 class="subtitle">{{ $subtitle }}</h2>
+            @endif
         </div>
     </div>
 </section>
