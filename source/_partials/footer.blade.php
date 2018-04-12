62f288caecca705389b21590fd1c83157e342c5a
Ranie Santos <ranietots32@gmail.com>
2018-04-12 16:02:43 +0800

    Fixed footer display bug

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer is-primary">
     <div class="container">
         <div class="content has-text-centered">
-            <p>All content copyright {{ $page->site->owner->name }} &copy; <span v-text="currentYear"/>. All rights reserved.</p>
+            <p>All content copyright {{ $page->site->owner->name }} &copy; <span v-text="currentYear"></span>. All rights reserved.</p>
             <p>Built with Jigsaw, hosted on GitHub Pages.</p>
         </div>
     </div>
